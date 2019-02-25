21a52dca969e97f51b0697b5cf89ba0fb921a108
michal <mibiel@student.agh.edu.pl>
2019-02-25 20:43:18 +0100

    EUPLSY-90: Rearrange import

--- service/src/main/java/com/lhsystems/module/datageneratorancillary/service/data/SeatGroup.java
+++ service/src/main/java/com/lhsystems/module/datageneratorancillary/service/data/SeatGroup.java
@@ -1,7 +1,6 @@
 package com.lhsystems.module.datageneratorancillary.service.data;
 
 import com.lhsystems.module.datageneratorancillary.service.serializer.data.ServiceSerializedEntity;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
